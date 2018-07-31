90824212a5c9b74828976d1c05bde22f4766e831
EC2 Default User <ec2-user@ip-172-31-13-70.us-east-2.compute.internal>
2018-07-31 11:37:28 +0000

    first commit

---
+++ urls.py
@@ -0,0 +1,29 @@
+"""MyTestApp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import include,url
+from . import views
+
+
+urlpatterns = [
+    url(r'^$',views.post_list,name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$',views.post_new,name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^drafts/$',views.post_draft_list,name="post_draft_list"),
+    url(r'^post/(?P<pk>\d+)/publish/$',views.post_publish,name="post_publish"),
+    url(r'^post//(?P<pk>\d+)/remove/$',views.post_remove,name="post_remove"),
+
+]
